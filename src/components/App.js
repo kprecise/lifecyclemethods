d74272c266a992101e2562032f34ced19ce356f3
Kyri Kyriacou <kkyriacou@architech.ca>
2019-12-02 17:18:39 -0500

    Re-worked lifecycle methods

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,69 @@
-import React from "react";
-import axios from 'axios';
-import Header from './header';
-import Nav from './nav';
-import Content from './content';
-import { Container, Row, Col, Alert } from 'reactstrap';
-import "./index.css";
+import React from 'react'
+import axios from 'axios'
+import {
+  Container, Row, Col, Alert
+} from 'reactstrap'
+import Content from './content'
+import './index.css'
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            pageReady: false
-        }
-        this.deletePhotos = this.deletePhotos.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+      pageReady: false,
+      photoAlbums: []
     }
+    this.deletePhotos = this.deletePhotos.bind(this)
+  }
 
-    componentDidMount() {
-        axios.get(`https://jsonplaceholder.typicode.com/albums/1/photos`)
-        .then(res => {
-          const photos = res.data;
-          this.setState({ photoAlbums: photos });
-        })        
-        setTimeout(
-            function() {
-                this.setState({
-                    pageReady: true
-                })
-            }
-            .bind(this),
-            1000
-        );
-    }
-    deletePhotos() {
-        this.setState(
-            { 
-                photoAlbums: undefined
-            }
-        );
-    }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/albums/1/photos')
+      .then((res) => {
+        const photos = res.data
+        this.setState({ photoAlbums: photos })
+      })
+    setTimeout(
+      () => {
+        this.setState({
+          pageReady: true
+        })
+      },
+      1000
+    )
+  }
 
-    render() {
-        const pageIsReady = this.state.pageReady;
-        const photoStatusChange = this.state.photoStatus;
-        return (
-            <Container>
-                <Row>
-                    <Col xs="12">
-                        <Header />
-                    </Col>
-                </Row>
-                { photoStatusChange &&
-                    <Row>
-                        <Alert color="danger">All photos have been removed.</Alert>
-                    </Row>
-                }
-                <Row>
-                    <Col xs="12" sm="6">       
-                        <Nav />
-                    </Col>
-                    <Col xs="12" sm="6">  
-                        { pageIsReady ? <Content photoAlbums={this.state.photoAlbums} deletePhotos={this.deletePhotos} /> : <Content loading="Content is loading........." />  }
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
+  deletePhotos() {
+    this.setState(
+      {
+        photoAlbums: undefined
+      }
+    )
+  }
+
+  render() {
+    const pageIsReady = this.state.pageReady
+    const photoStatusChange = this.state.photoStatus
+    return (
+      <Container>
+        <h1>Lifecycle Methods</h1>
+        { photoStatusChange
+              && (
+              <Row>
+                <Col xs="12">
+                  <Alert color="danger">All photos have been removed.</Alert>
+                </Col>
+              </Row>
+              )
+          }
+        <Row>
+          <Col xs="12">
+            { pageIsReady ? <Content photoAlbums={this.state.photoAlbums} deletePhotos={this.deletePhotos} /> : <Content loading="Content is loading........." /> }
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
 }
- 
-export default App;
+
+export default App
--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,62 +1,111 @@
-import React from "react";
-import { Container, Row, Col } from 'reactstrap';
-import "./index.scss";
+import React from 'react'
+import {
+  Row, Col, Button, Alert
+} from 'reactstrap'
+import './index.scss'
 
 class Content extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            photosStatus: false
-        }
-    }    
-    componentDidUpdate(prevProps) {
-        if(prevProps.photoAlbums !== this.props.photoAlbums) {
-            console.log('component updated')
-            this.setState({ photosStatus: true });
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      photoStatus: 'populated',
+      receivedProps: false
     }
-    render() {
-        const { photoAlbums, loading, deletePhotos } = this.props
-        const changedPhotoStatus = this.state.photosStatus
-        if (photoAlbums) {
-            const filterItems = photoAlbums.slice(0, 5)
-            return (
-                <div className="content">
-                    <Container>
-                    {
-                    filterItems.map(function(item) {
-                        return (
-                            <div key={item.id}>
-                                <Row>
-                                    <Col xs="9">
-                                        <h2>{item.title}</h2>
-                                    </Col>
-                                    <Col xs="3">
-                                        <img src={item.thumbnailUrl} />
-                                    </Col>                            
-                                </Row>  
-                            </div>
-                        )
-                    })
-                    }   
-                        <Row>
-                            <Col>
-                                <button onClick={deletePhotos}>Delete Photos</button>
-                            </Col>
-                        </Row>
-                    </Container>
-                </div>
-            )
-        } else if (changedPhotoStatus) { 
-            return (
-                <div>A property on this component has updated</div>
-            )
-        } 
-        else {
-            return (
-                <div>{loading}</div>
-            )
+    this.renderPhotosAlbums = this.renderPhotosAlbums.bind(this)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.photoAlbums === undefined && typeof prevProps.photoAlbums === 'object') {
+      this.setState(
+        {
+          photoStatus: 'empty'
         }
+      )
     }
-};
-export default Content;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps) {
+      this.setState(
+        {
+          receivedProps: true
+        }
+      )
+    }
+  }
+
+  renderPhotosAlbums() {
+    const {
+      photoAlbums,
+      deletePhotos
+    } = this.props
+
+    const filterItems = photoAlbums.slice(0, 5)
+    const filteredResult = filterItems.map((item) => {
+      return (
+        <Row className="item" key={item.id}>
+          <Col xs="9">
+            <h2>{item.title}</h2>
+            <span>ID: {item.id}</span>
+            <a href={item.url}>{item.url}</a>
+          </Col>
+          <Col xs="3">
+            <img src={item.thumbnailUrl} />
+          </Col>
+        </Row>
+      )
+    })
+    return (
+      <div>
+        {filteredResult}
+        <Row>
+          <Col>
+            <Button color="primary" onClick={deletePhotos}>Delete Photos</Button>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+
+  render() {
+    const {
+      photoAlbums,
+      loading
+    } = this.props
+
+    const isPhotoStatusEmpty = this.state.photoStatus === 'empty'
+    const didComponentReceiveProps = this.state.receivedProps
+    console.log('isPhotoStatusEmpty', isPhotoStatusEmpty)
+
+    return (
+      <div className="content">
+        {didComponentReceiveProps && (
+          <Row>
+            <Col xs={12}>
+              <Alert color="success">
+                Component received props
+              </Alert>
+            </Col>
+          </Row>
+        )
+        }
+        {
+            !photoAlbums ? (
+              <div>{loading}</div>
+            ) : this.renderPhotosAlbums()
+          }
+        { isPhotoStatusEmpty
+        && (
+        <Row>
+          <Col>
+            <Alert color="danger">All photos have been removed.</Alert>
+            <Alert color="warning">The status of the photo album has changed on this component. The photoAlbums was populated with data in prevProps but changed to empty in this.props</Alert>
+          </Col>
+        </Row>
+        )
+        }
+      </div>
+    )
+  }
+}
+export default Content
